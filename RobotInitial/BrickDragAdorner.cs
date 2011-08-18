d11a5417edec14542955b8bfda2e9511784cfcaa
ryan <ryan.thomann@gmail.com>
2011-08-18 23:51:18 +0930

    Hugo's ui prototype v02

---
+++ RobotInitial/BrickDragAdorner.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace RobotInitial 
+{
+    class BrickDragAdorner : Adorner
+    {
+        protected UIElement _child;
+        protected VisualBrush _brush;
+        protected UIElement _owner;
+        protected double XCenter;
+        protected double YCenter;
+
+        public BrickDragAdorner(UIElement owner) : base(owner) { }
+
+        public BrickDragAdorner(UIElement owner, UIElement adornElement, double opacity)
+            : base(owner)
+        {
+            System.Diagnostics.Debug.Assert(owner != null);
+            System.Diagnostics.Debug.Assert(adornElement != null); 
+            _owner = owner;
+
+			VisualBrush _brush = new VisualBrush(adornElement);
+			_brush.Opacity = opacity;
+
+			// As of now, assuming re have a rectangle (This will become a Brick proper)
+			Rectangle r = new Rectangle();
+			r.RadiusX = ((Rectangle)adornElement).RadiusX;
+			r.RadiusY = ((Rectangle)adornElement).RadiusY; ;
+			r.Width = ((Rectangle)adornElement).Width;
+			r.Height = ((Rectangle)adornElement).Height;
+
+			// Set the centre points
+			XCenter = r.Width / 2;
+			YCenter = r.Height / 2;
+
+			r.Fill = _brush;
+			_child = r;
+            
+        }
+
+
+        private double _leftOffset;
+        public double LeftOffset
+        {
+            get { return _leftOffset; }
+            set
+            {
+                _leftOffset = value - XCenter;
+                UpdatePosition();
+            }
+        }
+
+        private double _topOffset;
+        public double TopOffset
+        {
+            get { return _topOffset; }
+            set
+            {
+                _topOffset = value - YCenter;
+
+                UpdatePosition();
+            }
+        }
+
+        private void UpdatePosition()
+        {
+            AdornerLayer adorner = (AdornerLayer)this.Parent;
+            if (adorner != null)
+            {
+                adorner.Update(this.AdornedElement);
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _child;
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        protected override Size MeasureOverride(Size finalSize)
+        {
+            _child.Measure(finalSize);
+            return _child.DesiredSize;
+        }
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+            _child.Arrange(new Rect(_child.DesiredSize));
+            return finalSize;
+        }
+
+        public double scale = 1.0;
+        public override GeneralTransform GetDesiredTransform(GeneralTransform transform)
+        {
+            GeneralTransformGroup result = new GeneralTransformGroup();
+
+            result.Children.Add(base.GetDesiredTransform(transform));
+            result.Children.Add(new TranslateTransform(_leftOffset, _topOffset));
+            return result;
+        }
+    }
+}
--- RobotInitial/MainWindow.xaml.cs
+++ RobotInitial/MainWindow.xaml.cs
@@ -18,6 +18,12 @@ namespace RobotInitial
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		public FrameworkElement DragScope { get; set; }
+		private BrickDragAdorner brickAdorner = null;
+		private AdornerLayer adornLayer;
+		private bool _dragHasLeftScope = false;
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
@@ -29,12 +35,18 @@ namespace RobotInitial
 			if(e.Handled) return;
 
 			Panel panel = (Panel)sender;
-			UIElement element = (UIElement)e.Data.GetData("Object");
+
+			// AGAIN WE ASSUME THAT THIS IS A RECTANGLE
+			Rectangle element = (Rectangle)e.Data.GetData("Object");
 
 			if(panel != null && element != null) {
+				double x = e.GetPosition(panel).X;
+				double y = e.GetPosition(panel).Y;
 				panel.Children.Add(element);
-				element.SetValue(Canvas.LeftProperty,e.GetPosition(panel).X);
-				element.SetValue(Canvas.TopProperty, e.GetPosition(panel).Y);
+
+				// Note we are dropping from the centre of the brick
+				element.SetValue(Canvas.LeftProperty, x - (element.Width / 2));
+				element.SetValue(Canvas.TopProperty, y - (element.Height / 2));
 					
 				// set the value to return to the DoDragDrop call
 				e.Effects = DragDropEffects.Copy;
@@ -48,24 +60,68 @@ namespace RobotInitial
 
 		private void BrickDrag_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
 		{
-			// Set the brick button
-			UIElement brickButton = (UIElement)sender;
+			if(e.LeftButton == MouseButtonState.Pressed) {
+				// Set the brick button
+				UIElement brickButton = (UIElement)sender;
 
-			// Create the data object which will hold a brick in future
-			DataObject data = new DataObject();
+				// Create the data object which will hold a brick in future
+				DataObject data = new DataObject();
 
-			// TEMPORARY
-			if(sender is Rectangle) {
-				Rectangle rect = new Rectangle();
-				BrushConverter converter = new BrushConverter();
-				rect.Fill = ((Rectangle)brickButton).Fill;
-				rect.Width = 80;
-				rect.Height = 50;
-				data.SetData("Object",rect);
+				// TEMPORARY
+				if (sender is Rectangle)
+				{
+					Rectangle rect = new Rectangle();
+					BrushConverter converter = new BrushConverter();
+					rect.Fill = ((Rectangle)brickButton).Fill;
+					rect.Stroke = ((Rectangle)brickButton).Stroke;
+					rect.Width = 80;
+					rect.Height = 50;
+					rect.RadiusX = 4.0;
+					rect.RadiusY = 4.0;
+					
+					data.SetData("Object", rect);
+				
+
+					// Define the drag scope for the adorner
+					DragScope = Application.Current.MainWindow.Content as FrameworkElement;
+					System.Diagnostics.Debug.Assert(DragScope != null);
+
+					// Enable Drag & Drop in the scope so that we can use DragOver
+					bool previousDrop = DragScope.AllowDrop;
+					DragScope.AllowDrop = true;
+
+					// Set the drag event for the drag scope 
+					DragEventHandler draghandler = new DragEventHandler(DragScope_DragOver);
+					DragScope.PreviewDragOver += draghandler;
+
+					// Set the Drag Leave event handler for the drag scope 
+					DragEventHandler dragleavehandler = new DragEventHandler(DragScope_DragLeave);
+					DragScope.DragLeave += dragleavehandler;
+
+					// QueryContinue Drag goes with drag leave... 
+					QueryContinueDragEventHandler queryhandler = new QueryContinueDragEventHandler(DragScope_QueryContinueDrag);
+					DragScope.QueryContinueDrag += queryhandler;
+
+
+					// Create the BrickDragAdorner giving the parent and the child
+					brickAdorner = new BrickDragAdorner(DragScope, (UIElement)rect, 0.5);
+					adornLayer = AdornerLayer.GetAdornerLayer(DragScope as Visual);
+					adornLayer.Add(brickAdorner);
+
+					// Do the drag drop
+					DragDrop.DoDragDrop(brickButton, data, DragDropEffects.Move | DragDropEffects.Copy);
+
+					// Remove the Adorner from the layer
+					DragScope.AllowDrop = previousDrop;
+					AdornerLayer.GetAdornerLayer(DragScope).Remove(brickAdorner);
+					brickAdorner = null;
+
+					// Remove the handlers for the drag scope
+					DragScope.DragLeave -= dragleavehandler;
+					DragScope.QueryContinueDrag -= queryhandler;
+					DragScope.PreviewDragOver -= draghandler; 
+				}	
 			}
-
-			// Do the drag drop
-			DragDrop.DoDragDrop(brickButton, data, DragDropEffects.Move | DragDropEffects.Copy);
 		}
 
 		private void BrickDrag_GiveFeedback(object sender, System.Windows.GiveFeedbackEventArgs e)
@@ -73,6 +129,38 @@ namespace RobotInitial
 
 		}
 
+		private void DragScope_DragOver(object sender, DragEventArgs args)
+		{
+			if (brickAdorner != null)
+			{
+				brickAdorner.LeftOffset = args.GetPosition(DragScope).X /* - _startPoint.X */ ;
+				brickAdorner.TopOffset = args.GetPosition(DragScope).Y /* - _startPoint.Y */ ;
+			}
+		}
+
+		void DragScope_DragLeave(object sender, DragEventArgs e)
+		{
+			if (e.OriginalSource == DragScope)
+			{
+				Point p = e.GetPosition(DragScope);
+				Rect r = VisualTreeHelper.GetContentBounds(DragScope);
+				if (!r.Contains(p))
+				{
+					this._dragHasLeftScope = true;
+					e.Handled = true;
+				}
+			}
+		}
+
+		void DragScope_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+		{
+			if (this._dragHasLeftScope)
+			{
+				e.Action = DragAction.Cancel;
+				e.Handled = true;
+			}
+
+		}
 		private void openFileDialog(object sender, System.Windows.RoutedEventArgs e)
 		{
 			OpenFileDialog openFile = new OpenFileDialog();
