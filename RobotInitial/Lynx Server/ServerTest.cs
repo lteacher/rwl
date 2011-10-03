0a8e8b5ad2d29cdd0e421b08cb98a5525ee5e885
Hugo <meadow_fresh@hotmail.com>
2011-10-03 14:01:15 +1030

    Merged ctrls branch here
    Also commented out the Lynx test main for a second and also some extra defined exception.

--- RobotInitial/Lynx Server/ServerTest.cs
+++ RobotInitial/Lynx Server/ServerTest.cs
@@ -7,33 +7,33 @@ using System.Threading;
 
 namespace RobotInitial.Lynx_Server {
     class ServerTest {
-        static void Main() {
-            Thread ServerThread;
-            Lynx_Server server = new Lynx_Server();
+		//static void Main() {
+		//    Thread ServerThread;
+		//    Lynx_Server server = new Lynx_Server();
 
-            ServerThread = new Thread(server.start);
-            ServerThread.Start();
-            Console.Write("Server started \n");
-
-
-            StartBlock start = new StartBlock();
-
-            LoopBlock loop = new LoopBlock();
-            CountConditional count = new CountConditional();
-            count.Limit = 10;
-            loop.Condition = count;
-
-            MoveBlock truepath = new MoveBlock();
-            loop.LoopPath = truepath;
-            start.Next = loop;
-
-            Console.Write("Contacting Server \n");
-            Network.connectToLynx(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7331), start);
+		//    ServerThread = new Thread(server.start);
+		//    ServerThread.Start();
+		//    Console.Write("Server started \n");
 
 
-            //Keep Console Open
-            while (true) {
-            }
-        }
+		//    StartBlock start = new StartBlock();
+
+		//    LoopBlock loop = new LoopBlock();
+		//    CountConditional count = new CountConditional();
+		//    count.Limit = 10;
+		//    loop.Condition = count;
+
+		//    MoveBlock truepath = new MoveBlock();
+		//    loop.LoopPath = truepath;
+		//    start.Next = loop;
+
+		//    Console.Write("Contacting Server \n");
+		//    Network.connectToLynx(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7331), start);
+
+
+		//    //Keep Console Open
+		//    while (true) {
+		//    }
+		//}
     }
 }
