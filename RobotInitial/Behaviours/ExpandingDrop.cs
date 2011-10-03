0866e65a880d518922878fba0ee62d5eacf657e3
Hugo <meadow_fresh@hotmail.com>
2011-10-03 12:15:44 +1030

    Fixing expand for switch block, still bugged doesnt quite work but uploading changes anyhow

--- RobotInitial/Behaviours/ExpandingDrop.cs
+++ RobotInitial/Behaviours/ExpandingDrop.cs
@@ -13,26 +13,23 @@ using System.Windows.Interactivity;
 using RobotInitial.ViewModel;
 using RobotInitial.View;
 using RobotInitial.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
 //using Microsoft.Expression.Interactivity.Core;
 
 namespace RobotInitial.Behaviours
 {
 	public class BlockExpand : Behavior<UIElement>
 	{
+		private WorkspaceView workspace;
+		private FrameworkElement parent;
+		private Collection<SwitchControlBlockView> switchBlockViews;
+		private double maxChildHeight;
+		private int depth = 0;
+
 		public BlockExpand()
 		{
-			// Insert code required on object creation below this point.
-
-			//
-			// The line of code below sets up the relationship between the command and the function
-			// to call. Uncomment the below line and add a reference to Microsoft.Expression.Interactions
-			// if you choose to use the commented out version of MyFunction and MyCommand instead of
-			// creating your own implementation.
-			//
-			// The documentation will provide you with an example of a simple command implementation
-			// you can use instead of using ActionCommand and referencing the Interactions assembly.
-			//
-			//this.MyCommand = new ActionCommand(this.MyFunction);
+			
 		}
 
 		protected override void OnAttached()
@@ -53,6 +50,58 @@ namespace RobotInitial.Behaviours
 			// Insert code that you would want run when the Behavior is removed from an object.
 		}
 
+		// Setup the workspace by finding it as the parent
+		private void setWorkspaceAndParent(FrameworkElement dropTarget, FrameworkElement child)
+		{
+			// if the child to be added is bigger than the parent then
+			// the 'Root' parent must refit to the workspace, this
+			// means we need the root control so now we have to find it!
+			FrameworkElement treeParent = (FrameworkElement)VisualTreeHelper.GetParent(dropTarget);
+			switchBlockViews = new Collection<SwitchControlBlockView>();
+
+			// Find the ultimate parent
+			double tempChildHeight = child.DesiredSize.Height;
+			maxChildHeight = tempChildHeight;
+			depth = 0;
+			while (treeParent != null)
+			{
+				treeParent = (FrameworkElement)VisualTreeHelper.GetParent(treeParent);
+				if (treeParent == null) break;
+				if (treeParent.GetType() == typeof(LoopControlBlockView) || treeParent.GetType() == typeof(SwitchControlBlockView))
+				{
+					depth++;
+					if (tempChildHeight > maxChildHeight) maxChildHeight = tempChildHeight;
+					tempChildHeight = treeParent.DesiredSize.Height;
+					if (((FrameworkElement)dropTarget.Parent).GetType() == typeof(LoopControlBlockView))
+					{
+						if (treeParent.GetType() == typeof(LoopControlBlockView))
+						{
+							parent = treeParent;
+						}
+					}
+					else if (((FrameworkElement)dropTarget.Parent).GetType() == typeof(SwitchControlBlockView))
+					{
+						if (treeParent.GetType() == typeof(SwitchControlBlockView))
+						{
+							parent = treeParent;
+						}
+					}
+				}
+
+				// If a switch is found in the heirachy
+				if (treeParent.GetType() == typeof(SwitchControlBlockView)) {
+					// Add the switch view model to the collection for later size check
+					Console.WriteLine("!!! ADDING A SWITCH !!!");
+					switchBlockViews.Add((SwitchControlBlockView)treeParent);
+				}
+
+				if (treeParent.GetType() == typeof(WorkspaceView)) {
+					workspace = (WorkspaceView)treeParent;
+					break;
+				}
+			}
+		}
+
 		private void Item_Dropped(object sender, System.Windows.DragEventArgs e)
 		{
 			FrameworkElement dropTarget = (FrameworkElement)sender;
@@ -61,56 +110,118 @@ namespace RobotInitial.Behaviours
 			FrameworkElement dropSource = (FrameworkElement)e.Data.GetData("Object");
 			ControlBlockViewModel sourceViewModel = (ControlBlockViewModel)dropSource.DataContext;
 			if(sourceViewModel.Type.Equals("Move"))
-			{	
+			{
+				// Create a new Move block as the child
+				MoveControlBlockView child = new MoveControlBlockView();
+
+				// Set the workspace and the parent
+				setWorkspaceAndParent(dropTarget,child);
+
 				if (targetViewModel.GetType() == typeof(LoopControlBlockViewModel))
 				{
-					((LoopControlBlockViewModel)targetViewModel).AddChildBlock((new MoveControlBlockView()));
+					((LoopControlBlockViewModel)targetViewModel).AddChildBlock(child);
 				}
 				else if (targetViewModel.GetType() == typeof(SwitchControlBlockViewModel))
 				{
-					((LoopControlBlockViewModel)targetViewModel).AddChildBlock((new MoveControlBlockView()));
+					// Set the parent for the child for checking the Y value
+					FrameworkElement dropParent = (FrameworkElement)dropTarget.Parent;
 
+					// Set the parent Y mouse value as relevant to the workspace or the current target
+					double parentY = depth == 1 ? e.GetPosition(workspace).Y : e.GetPosition(dropTarget).Y;
+
+					// Set the top value depending on the current depth since the top will be
+					// different for a component on the workspace compared to one inside another component
+					double top = depth == 1 ? (double)dropTarget.Parent.GetValue(Canvas.TopProperty) : 0;
+
+					// If the mouse Y location is in the top or bottom half of the component when dropped
+					if (parentY > top + (dropParent.RenderSize.Height / 2))
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_BOTTOM);
+					}
+					else
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_TOP);
+					}
 				}
 			}
 			if (sourceViewModel.Type.Equals("Loop"))
 			{
+				// Create a new Loop block as the child
 				LoopControlBlockView child = new LoopControlBlockView();
+
+				// Set the workspace and the parent
+				setWorkspaceAndParent(dropTarget, child);
+
 				if (targetViewModel.GetType() == typeof(LoopControlBlockViewModel)) {
 					((LoopControlBlockViewModel)targetViewModel).AddChildBlock(child);
 				}
 				else if(targetViewModel.GetType() == typeof(SwitchControlBlockViewModel)) {
-					double top = (double)dropTarget.Parent.GetValue(Canvas.TopProperty);
-
+					// Set the parent for the child for checking the Y value
 					FrameworkElement dropParent = (FrameworkElement)dropTarget.Parent;
 
-					Console.Write("E getPosition: {0}, Top: {2},  target rendersize: {1}", e.GetPosition(dropTarget).Y,  (dropParent.ActualHeight / 2) , top);
+					// Set the parent Y mouse value as relevant to the workspace or the current target
+					double parentY = depth == 1 ? e.GetPosition(workspace).Y : e.GetPosition(dropTarget).Y;
 
-					if (e.GetPosition(dropTarget).Y > top + (dropParent.RenderSize.Height / 2)) {
+					// Set the top value depending on the current depth since the top will be
+					// different for a component on the workspace compared to one inside another component
+					double top = depth == 1 ? (double)dropTarget.Parent.GetValue(Canvas.TopProperty) : 0;
+
+					// If the mouse Y location is in the top or bottom half of the component when dropped
+					if (parentY > top + (dropParent.RenderSize.Height / 2))
+					{
 						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_BOTTOM);
-					} else {
+					}
+					else
+					{
 						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_TOP);
 					}
-
 				}
 
 				// Expand the block
 				ExpandControlBlock(dropTarget,child);
-
 			}
 			if (sourceViewModel.Type.Equals("Wait"))
 			{
+				// Create a new Wait block as the child
+				WaitControlBlockView child = new WaitControlBlockView();
+
+				// Set the workspace and the parent
+				setWorkspaceAndParent(dropTarget, child);
+
 				if (targetViewModel.GetType() == typeof(LoopControlBlockViewModel))
 				{
-					((LoopControlBlockViewModel)targetViewModel).AddChildBlock((new WaitControlBlockView()));
+					((LoopControlBlockViewModel)targetViewModel).AddChildBlock(child);
 				}
 				else if (targetViewModel.GetType() == typeof(SwitchControlBlockViewModel))
 				{
-					((LoopControlBlockViewModel)targetViewModel).AddChildBlock((new WaitControlBlockView()));
+					// Set the parent for the child for checking the Y value
+					FrameworkElement dropParent = (FrameworkElement)dropTarget.Parent;
+
+					// Set the parent Y mouse value as relevant to the workspace or the current target
+					double parentY = depth == 1 ? e.GetPosition(workspace).Y : e.GetPosition(dropTarget).Y;
+
+					// Set the top value depending on the current depth since the top will be
+					// different for a component on the workspace compared to one inside another component
+					double top = depth == 1 ? (double)dropTarget.Parent.GetValue(Canvas.TopProperty) : 0;
+
+					// If the mouse Y location is in the top or bottom half of the component when dropped
+					if (parentY > top + (dropParent.RenderSize.Height / 2))
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_BOTTOM);
+					}
+					else
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_TOP);
+					}
 				}
 			}
 			if (sourceViewModel.Type.Equals("Switch"))
 			{
+				// Create a new Switch block as the child
 				SwitchControlBlockView child = new SwitchControlBlockView();
+
+				// Set the workspace and the parent
+				setWorkspaceAndParent(dropTarget, child);
 
 				if (targetViewModel.GetType() == typeof(LoopControlBlockViewModel))
 				{
@@ -118,13 +229,28 @@ namespace RobotInitial.Behaviours
 				}
 				else if (targetViewModel.GetType() == typeof(SwitchControlBlockViewModel))
 				{
-					((LoopControlBlockViewModel)targetViewModel).AddChildBlock(child);
+					// Set the parent for the child for checking the Y value
+					FrameworkElement dropParent = (FrameworkElement)dropTarget.Parent;
+
+					// Set the parent Y mouse value as relevant to the workspace or the current target
+					double parentY = depth == 1 ? e.GetPosition(workspace).Y : e.GetPosition(dropTarget).Y;
+
+					// Set the top value depending on the current depth since the top will be
+					// different for a component on the workspace compared to one inside another component
+					double top = depth == 1 ? (double)dropTarget.Parent.GetValue(Canvas.TopProperty) : 0;
+
+					// If the mouse Y location is in the top or bottom half of the component when dropped
+					if (parentY > top + (dropParent.RenderSize.Height / 2))
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_BOTTOM);
+					}
+					else
+					{
+						((SwitchControlBlockViewModel)targetViewModel).AddChildBlock(child, SwitchControlBlockViewModel.ORIENTATION_TOP);
+					}
 				}
-
-
 				// TODO, This is wrong switch not implemented
 				ExpandControlBlock(dropTarget, child);
-
 			}
 			e.Handled = true;
 		}
@@ -149,56 +275,24 @@ namespace RobotInitial.Behaviours
 			// Setup the measured size
 			child.Measure(new Size(child.MaxWidth, child.MaxHeight));
 
-			// if the child to be added is bigger than the parent then
-			// it the 'Root' parent must refit to the workspace, this
-			// means we need the root control so now we have to find it!
-			FrameworkElement parent = (FrameworkElement)dropTarget.Parent;
-			FrameworkElement treeParent = (FrameworkElement)VisualTreeHelper.GetParent(dropTarget);
-
-			// Find the ultimate parent
-			double tempChildHeight = child.DesiredSize.Height;
-			double maxChildHeight = tempChildHeight;
-			int parentCount = 0;
-			while (treeParent != null)
-			{
-				treeParent = (FrameworkElement)VisualTreeHelper.GetParent(treeParent);
-				if (treeParent == null) break;
-				if (treeParent.GetType() == typeof(LoopControlBlockView) || treeParent.GetType() == typeof(SwitchControlBlockView))
-				{
-					parentCount++;
-					if (tempChildHeight > maxChildHeight) maxChildHeight = tempChildHeight;	
-					tempChildHeight = treeParent.DesiredSize.Height;
-					if(((FrameworkElement)dropTarget.Parent).GetType() == typeof(LoopControlBlockView)) {
-						if (treeParent.GetType() == typeof(LoopControlBlockView))
-						{
-							parent = treeParent;
-						}
-					}
-					else if (((FrameworkElement)dropTarget.Parent).GetType() == typeof(SwitchControlBlockView))
-					{
-						if (treeParent.GetType() == typeof(SwitchControlBlockView))
-						{
-							parent = treeParent;
-						}
-					}
-				}
-				if (treeParent.GetType() == typeof(WorkspaceView)) break;
+			// Setup the component height for comparison
+			double parentHeight = 0;
+			if ( ((FrameworkElement)dropTarget.Parent).GetType() == typeof(LoopControlBlockView) ) {
+				parentHeight = ((FrameworkElement)dropTarget.Parent).RenderSize.Height;
+			} else if( ((FrameworkElement)dropTarget.Parent).GetType() == typeof(SwitchControlBlockView) ) {
+				parentHeight = ((FrameworkElement)dropTarget.Parent).RenderSize.Height / 2;
 			}
 
 			// If the parent height is less than or the same as the child
-			Console.WriteLine("Parent Height: {0}, Child Height: {1}, MaxCHild: {2}", parent.RenderSize.Height, child.DesiredSize.Height, maxChildHeight);
-
-			if (((FrameworkElement)dropTarget.Parent).RenderSize.Height <= child.DesiredSize.Height)
+			if (parentHeight <= child.DesiredSize.Height)
 			{
-				Console.WriteLine("===>>>>  MOVING UP");
-
 				// Get the top point of the root parent
 				double top = Canvas.GetTop(parent);
 
 				// If the parent size minus the new child size will have less than
 				// the minimum top+bottom buffer(50), meaning the child is bigger than the main parent
 				if (!(parent.RenderSize.Height - child.DesiredSize.Height >= 50)) {
-					double offset = ((child.DesiredSize.Height+(parentCount * 50)) - parent.RenderSize.Height)/2;
+					double offset = ((child.DesiredSize.Height+(depth * 50)) - parent.RenderSize.Height)/2;
 					top = top - offset;
 				}
 
@@ -206,18 +300,32 @@ namespace RobotInitial.Behaviours
 				else if(child.DesiredSize.Height >= maxChildHeight) {
 					// Make sure that when the max child expands it will have space!
 					// e.g. the parent will fit the max size plus its buffer!
-					if(child.DesiredSize.Height+(parentCount*50) > parent.RenderSize.Height) {
-						double offset = ((child.DesiredSize.Height + (parentCount * 50)) - parent.RenderSize.Height) / 2;
+					if(child.DesiredSize.Height+(depth*50) > parent.RenderSize.Height) {
+						double offset = ((child.DesiredSize.Height + (depth * 50)) - parent.RenderSize.Height) / 2;
 						top = top - offset;
 					}
-
 				}
 				else if (maxChildHeight + (50) >= parent.RenderSize.Height) {
-					double offset = ((child.DesiredSize.Height + (parentCount * 50)) - parent.RenderSize.Height) / 2;
+					double offset = ((child.DesiredSize.Height + (depth * 50)) - parent.RenderSize.Height) / 2;
 					top = top - offset;
 				}
 
+				// Set the top value
 				parent.SetValue(Canvas.TopProperty, top);
+
+				parent.Dispatcher.BeginInvoke(DispatcherPriority.Render, new DispatcherOperationCallback(delegate(Object state) {
+					resizeSwitchBlocks();
+					return null;
+				}),null);
+			}
+		}
+
+		private void resizeSwitchBlocks() {
+			// Update the switches that were affected!
+			foreach (SwitchControlBlockView switchView in switchBlockViews)
+			{
+				Console.WriteLine("***************** DOING RESIZE *****************");
+				((SwitchControlBlockViewModel)switchView.DataContext).CheckChildrenSizes();
 			}
 		}
 
--- RobotInitial/ViewModel/SwitchControlBlockViewModel.cs
+++ RobotInitial/ViewModel/SwitchControlBlockViewModel.cs
@@ -6,57 +6,49 @@ using System.Windows;
 using System.Collections.ObjectModel;
 using System.Windows.Shapes;
 using System.Windows.Media;
+using System.ComponentModel;
 
 namespace RobotInitial.ViewModel
 {
-	class SwitchControlBlockViewModel : ControlBlockViewModel
+	class SwitchControlBlockViewModel : ControlBlockViewModel, INotifyPropertyChanged
 	{
 		
 		public const int ORIENTATION_TOP = 1;
 		public const int ORIENTATION_BOTTOM = 0;
-		private ObservableCollection<UIElement> _childrenTop = new ObservableCollection<UIElement>();
-		private ObservableCollection<UIElement> _childrenBottom = new ObservableCollection<UIElement>();
+		private double _stackHeight = 225;
+		private double topMaxChildHeight = 0;
+		private double bottomMaxChildHeight = 0;
+		private ObservableCollection<FrameworkElement> _childrenTop = new ObservableCollection<FrameworkElement>();
+		private ObservableCollection<FrameworkElement> _childrenBottom = new ObservableCollection<FrameworkElement>();
 
 		public SwitchControlBlockViewModel() {
 			Type = "Switch";
-
-
-			Rectangle r = new Rectangle();
-			r.Fill = Brushes.Black;
-			r.Width = 75;
-			r.Height = 75;
-			r.Margin = new Thickness(25, 0, 25, 0);
-			r.HorizontalAlignment = HorizontalAlignment.Center;
-			r.VerticalAlignment = VerticalAlignment.Center;
-
-			Rectangle v = new Rectangle();
-			v.Fill = Brushes.Red;
-			v.Width = 75;
-			v.Height = 75;
-			v.Margin = new Thickness(25, 0, 25, 0);
-			v.HorizontalAlignment = HorizontalAlignment.Center;
-			v.VerticalAlignment = VerticalAlignment.Center;
-
-			Rectangle z = new Rectangle();
-			z.Fill = Brushes.Green;
-			z.Width = 75;
-			z.Height = 75;
-			z.Margin = new Thickness(25, 0, 25, 0);
-			z.HorizontalAlignment = HorizontalAlignment.Center;
-			z.VerticalAlignment = VerticalAlignment.Center;
-
-			AddChildBlock(r,1);
-			AddChildBlock(v,0);
-			AddChildBlock(z,0);
-
 		}
 
 		public Thickness TopStackMargin
 		{
 			get
 			{
-				if (ChildrenTop.Count == 0 && ChildrenBottom.Count == 0) return new Thickness(0, 25, 0, 25);
-				return new Thickness(25, 25, 25, 12.5);
+				// If the bottom children stack is empty OR both child heights are equal
+				// OR if the top margin is the max, set the default margin
+				if (ChildrenBottom.Count == 0 ||
+					bottomMaxChildHeight == topMaxChildHeight || 
+					topMaxChildHeight > bottomMaxChildHeight) return new Thickness(0, 25, 0, 12.5);
+
+				// The bottom is not empty. If its max child height is greater than
+				// the top max child height, use that child for setting the margin
+				if (bottomMaxChildHeight > topMaxChildHeight) {
+					double margin = (bottomMaxChildHeight - topMaxChildHeight) / 2 + 25;
+					Console.WriteLine("===>> TopStack: Bottom greater, margin is: {0}", margin);
+					return new Thickness(0, margin, 0, margin - 12.5);
+				}
+				// Otherwise return the margin using the top child height
+				else
+				{
+					double margin = (topMaxChildHeight - bottomMaxChildHeight) / 2 + 25;
+					Console.WriteLine("===>> TopStack: Top greater, margin is: {0}", margin);
+					return new Thickness(0, margin, 0, margin - 12.5);
+				}
 			}
 		}
 
@@ -64,25 +56,57 @@ namespace RobotInitial.ViewModel
 		{
 			get
 			{
-				if (ChildrenTop.Count == 0 && ChildrenBottom.Count == 0) return new Thickness(0, 25, 0, 25);
-				return new Thickness(25, 12.5, 25, 25);
+				// If the top children stack is empty OR both child heights are equal
+				// OR if the bottom margin is the max, set the default margin
+				if (ChildrenTop.Count == 0 || 
+					bottomMaxChildHeight == topMaxChildHeight ||
+					bottomMaxChildHeight > topMaxChildHeight) return new Thickness(0, 12.5, 0, 25);
+
+				// The top is not empty. If its max child height is greater than
+				// the top max child height, use that child for setting the margin
+				if (topMaxChildHeight > bottomMaxChildHeight) {
+					double margin = (topMaxChildHeight - bottomMaxChildHeight) / 2 + 25;
+					//Console.WriteLine("===>> BottomStack: Top greater, margin is: {0}", margin);
+					//Console.WriteLine("topMaxChildHeight: {0}, bottomMaxChildHeight: {1}", topMaxChildHeight, bottomMaxChildHeight);
+					return new Thickness(0, margin - 12.5, 0, margin);
+				}
+
+				// Otherwise return the margin using the bottom child height
+				else {
+					double margin = (bottomMaxChildHeight - topMaxChildHeight) / 2 + 25;
+					//Console.WriteLine("===>> BottomStack: Bottom greater, margin is: {0}", margin);
+					return new Thickness(0, margin - 12.5, 0, margin);
+				}
 			}
 		}
 
-		public ObservableCollection<UIElement> ChildrenTop
+		public double StackHeight {
+			get
+			{
+				return _stackHeight;
+			}
+			set
+			{
+				_stackHeight = value;
+				NotifyPropertyChanged("StackHeight");
+			}
+		}
+
+		public ObservableCollection<FrameworkElement> ChildrenTop
 		{
 			get { return _childrenTop; }
 		}
 
-		public ObservableCollection<UIElement> ChildrenBottom
+		public ObservableCollection<FrameworkElement> ChildrenBottom
 		{
 			get { return _childrenBottom; }
 		}
 
 		public void AddChildBlock(FrameworkElement element, int orientation)
 		{
+			element.Measure(new Size(element.MaxWidth, element.MaxHeight));
+
 			if(orientation == ORIENTATION_TOP) {
-				Console.Write("BLOCK ADD ");
 				if (ChildrenTop.Count == 0)
 				{
 					element.Margin = new Thickness(50, 0, 50, 0);
@@ -91,8 +115,14 @@ namespace RobotInitial.ViewModel
 				{
 					element.Margin = new Thickness(-25, 0, 50, 0);
 				}
+
+				// Check the new element's size to possibly set a new max child height
+				if (element.DesiredSize.Height > topMaxChildHeight) topMaxChildHeight = element.DesiredSize.Height;
+
 				// Add a child to the collection
-				_childrenTop.Add(element);
+				ChildrenTop.Add(element);
+				NotifyPropertyChanged("BottomStackMargin");
+				NotifyPropertyChanged("TopStackMargin");
 			}
 			if (orientation == ORIENTATION_BOTTOM)
 			{
@@ -104,11 +134,61 @@ namespace RobotInitial.ViewModel
 				{
 					element.Margin = new Thickness(-25, 0, 50, 0);
 				}
-				// Add a child to the collection
-				_childrenBottom.Add(element);
-			}
 
-			
+				// Check the new element's size to possibly set a new max child height
+				if (element.DesiredSize.Height > bottomMaxChildHeight) bottomMaxChildHeight = element.DesiredSize.Height;
+
+				// Add a child to the collection
+				ChildrenBottom.Add(element);
+				NotifyPropertyChanged("TopStackMargin");
+				NotifyPropertyChanged("BottomStackMargin");
+			}
+		}
+
+		// This horrible method checks children sizes to update when
+		// internal collections change (called in ExpandingDrop, ONE of these 
+		// sets WILL be updating, and it should only be once)
+		public void CheckChildrenSizes() {
+			// Check the top children
+			foreach (FrameworkElement element in ChildrenTop)
+			{
+				Console.WriteLine("** Top ** Max Height: {0}, Replacement: {1}", topMaxChildHeight, element.RenderSize.Height);
+
+				if(element.RenderSize.Height > topMaxChildHeight) {
+
+					topMaxChildHeight = element.RenderSize.Height;
+					NotifyPropertyChanged("TopStackMargin");
+					NotifyPropertyChanged("BottomStackMargin");
+					return;
+				}
+			}
+			// Check the bottom children
+			foreach (FrameworkElement element in ChildrenBottom)
+			{
+				Console.WriteLine("** Bottom ** Max Height: {0}, Replacement: {1}", bottomMaxChildHeight, element.DesiredSize.Height);
+				if (element.RenderSize.Height > bottomMaxChildHeight)
+				{
+					
+					bottomMaxChildHeight = element.RenderSize.Height;
+					NotifyPropertyChanged("TopStackMargin");
+					NotifyPropertyChanged("BottomStackMargin");
+					return;
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Notifies the property changed.
+		/// </summary>
+		/// <param name="property">The property.</param>
+		private void NotifyPropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
 		}
 	}
 }
