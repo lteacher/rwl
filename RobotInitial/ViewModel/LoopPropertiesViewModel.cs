4426b6561e37eb03b466b39b37be34c4d5f7b431
seth <sethpink@gmail.com>
2011-10-27 01:48:40 +1030

    fixed merge conflict

--- RobotInitial/ViewModel/LoopPropertiesViewModel.cs
+++ RobotInitial/ViewModel/LoopPropertiesViewModel.cs
@@ -53,25 +53,25 @@ namespace RobotInitial.ViewModel {
 					// Check which condition selected
 					switch (value) {
 						case 0: // IR Sensor - Front
-							_irSensor.ListeningPort = value;
+							_irSensor.IRSensorNumber = value;
 							break;
 						case 1: // IR Sensor - Front Left
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 						case 2: // IR Sensor - Front Right
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 						case 3: // IR Sensor - Rear
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 						case 4: // IR Sensor - Rear Left
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 						case 5: // IR Sensor - Rear Right
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 						case 6: // Forever
-							_irSensor.ListeningPort = value;
+                            _irSensor.IRSensorNumber = value;
 							break;
 					}
 				}
