b159212a2308fef50ecc65ec61bd2f6dddc03667
Hugo <meadow_fresh@hotmail.com>
2011-10-29 04:12:40 +1030

    Added a behaviour to center the scrollviewer on the content when the program starts and resizes.
    (yes apparently a whole behaviour was needed for this!)

---
+++ RobotInitial/Behaviours/ScrollViewerCentering.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Interactivity;
+using System.Windows.Controls;
+using System.Windows;
+using RobotInitial.ViewModel;
+using RobotInitial.View;
+
+namespace RobotInitial.Behaviours {
+	class ScrollViewerCentering : Behavior<ScrollViewer> {
+		
+		public ScrollViewerCentering()
+		{
+			// Insert code required on object creation below this point.
+		}
+
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			AssociatedObject.SizeChanged +=new SizeChangedEventHandler(AssociatedObject_SizeChanged);
+		}
+
+		private void AssociatedObject_SizeChanged(object sender, SizeChangedEventArgs e) {
+			WorkspaceView workspaceView = (WorkspaceView)AssociatedObject.Parent;
+			WorkspaceViewModel workspaceViewModel = (WorkspaceViewModel)workspaceView.DataContext;
+			double offset = workspaceViewModel.SequenceY;
+			AssociatedObject.ScrollToVerticalOffset(offset/2+12.5);
+		}
+
+	}
+}
